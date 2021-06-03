0f6462c042220356f8bf4af5bab7806a77836834
fabriziovigevani <fabriziovigevani@gmail.com>
2021-06-03 18:07:41 -0300

    Refactor pool provider

--- src/providers/Poolprovider.js
+++ src/providers/Poolprovider.js
@@ -1,7 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import tulipData from 'tulip-backend'
-import { getContract } from '../web3-contracts'
+import { getContract, useContractReadOnly } from '../web3-contracts'
 import { getNetworkConfig } from '../networks'
 import honeyFarm from '../abi/honeyfarm.json'
 import erc20 from '../abi/ERC20.json'
@@ -11,49 +16,55 @@ import { providers as Providers } from 'ethers'
 const PoolContext = React.createContext()
 
 export function PoolProvider({ children }) {
-  const tokens = []
   const [balance, setBalance] = useState()
-  // const [balance, setBalance] = useState('')
-  // const [deposits, setDeposits] = useState('')
+  const [depositData, setDepositData] = useState([])
+  const [poolData, setPoolData] = useState([])
+  const [poolInfo, setPoolInfo] = useState()
 
   const {
     account,
     _web3ReactContext: { chainId },
   } = useWallet()
-
   const network = getNetworkConfig(chainId)
 
-  let contract
-  if (chainId !== undefined) {
-    contract = getContract(network.honeyfarm, honeyFarm)
-  }
-  const loadPoolData = async () => {
+  const contract = useContractReadOnly(network.honeyfarm, honeyFarm)
+
+  const loadPoolData = useCallback(async () => {
+    const tokens = []
     let tulipApy = []
     if (chainId !== undefined && account) {
       tulipApy = await tulipData.farm.apys({ chain_id: chainId })
       tulipApy.forEach(pool => {
         tokens.push(pool.pair)
       })
+
+      let tulipD = []
+      if (tokens.length > 0) {
+        tulipD = await tulipData.wallet.simplyTokenBalances({
+          user_address: account,
+          chain_id: chainId,
+          tokens: tokens,
+          web3: {
+            eth: new Providers.Web3Provider(window.ethereum),
+          },
+        })
+        setBalance(tulipD)
+      }
     }
-    let tulipD = []
-    if (chainId !== undefined && account && tokens.length > 0) {
-      tulipD = await tulipData.wallet.simplyTokenBalances({
-        user_address: account,
-        chain_id: chainId,
-        tokens: tokens,
-        web3: {
-          eth: new Providers.Web3Provider(window.ethereum),
-        },
-      })
-      setBalance(tulipD)
+
+    setPoolData(tulipApy)
+  }, [account, chainId])
+
+  const loadPoolInfo = useCallback(async () => {
+    try {
+      const poolInfo = await tulipData.farm.info({ chain_id: chainId })
+      setPoolInfo(poolInfo)
+    } catch (err) {
+      console.error(`Error fetching pool info : ${err}`)
     }
-    return tulipApy
-  }
-  const loadPoolInfo = async () => {
-    const poolInfo = await tulipData.farm.info({ chain_id: chainId })
-    return poolInfo
-  }
-  const loadDepositData = async () => {
+  }, [chainId])
+
+  const loadDepositData = useCallback(async () => {
     const deposits = []
     if (account) {
       const tulipF = await tulipData.farm.deposits({
@@ -71,38 +82,49 @@ export function PoolProvider({ children }) {
             rewardBalance,
           })
         }
-        return deposits
-      } else {
-        return []
+        setDepositData(deposits)
+        return
       }
     }
-  }
-  const poolData = useQuery('loadPoolData', loadPoolData)
-  const poolInfo = useQuery('loadPoolInfo', loadPoolInfo)
-  const deposits = useQuery('loadDepositData', loadDepositData)
+    setDepositData([])
+  }, [account, chainId, contract])
+
+  const fetchData = useCallback(() => {
+    loadDepositData()
+    loadPoolData()
+    loadPoolInfo()
+  }, [loadDepositData, loadPoolData, loadPoolInfo])
 
   useEffect(() => {
-    poolData.refetch()
-    poolInfo.refetch()
-    deposits.refetch()
-  }, [account])
+    if (account) {
+      fetchData()
+    }
+  }, [account, fetchData])
 
-  if (contract !== undefined) {
-    contract.on('Transfer', (to, amount, from) => {
-      if (account) {
-        poolData.refetch()
-        poolInfo.refetch()
-        deposits.refetch()
+  useEffect(() => {
+    if (account && contract) {
+      contract.on('Transfer', (to, amount, from) => {
+        fetchData()
+      })
+    }
+
+    return () => {
+      if (contract) {
+        contract.off('Transfer')
       }
-    })
-  }
-  const r = {
-    data: poolData.data,
-    status,
-    balance,
-    deposits: deposits.data,
-    poolInfo: poolInfo.data,
-  }
+    }
+  }, [account, contract, fetchData])
+
+  const r = useMemo(
+    () => ({
+      balance,
+      data: poolData,
+      deposits: depositData,
+      poolInfo: poolInfo,
+    }),
+    [balance, depositData, poolData, poolInfo]
+  )
+
   return <PoolContext.Provider value={r}>{children}</PoolContext.Provider>
 }
 
