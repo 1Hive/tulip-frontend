aa59d485b1f2461c6e9a90bf43caae777f5ca76c
jupiteruwdev <harutoinoue8@gmail.com>
2021-06-30 22:37:20 -0500

    havest all ui updated.

--- src/components/Farming/DepositTable/index.js
+++ src/components/Farming/DepositTable/index.js
@@ -14,6 +14,7 @@ import { getNetworkConfig } from '../../../networks'
 import UserErrorScreen from '../../Errors/UserErrorScreen'
 import { truncateDecimals } from '../../../lib/math-utils'
 import styled from 'styled-components'
+import HarvestAll from '../HavestAll'
 
 const DepositTable = props => {
   const {
@@ -101,6 +102,33 @@ const DepositTable = props => {
     setErrorVisible(false)
   }
 
+  const wrapBatchAction = items => {
+    let dollarSum = 0
+    let amountSum = 0
+    let rewardBalanceSum = 0
+    items.forEach(
+      item => (dollarSum += Number(calculateDollar(item.amount, item.pairInfo)))
+    )
+    items.forEach(item => (amountSum += Number(item.amount)))
+    items.forEach(
+      item =>
+        (rewardBalanceSum += Number(
+          truncateDecimals(Number(item.rewardBalance) / 1e18)
+        ))
+    )
+    return [
+      ...items,
+      {
+        amount: {
+          amountSum,
+          dollarSum,
+        },
+        rewardBalance: rewardBalanceSum,
+        unlockTime: '-',
+      },
+    ]
+  }
+
   return (
     <div
       css={`
@@ -127,7 +155,6 @@ const DepositTable = props => {
           'Unlock Date',
           'Reward Asset',
           'Reward Balance',
-          '',
           '',
         ]}
         css={`
@@ -158,7 +185,13 @@ const DepositTable = props => {
             clearLabel: null,
           },
         }}
-        entries={account ? (props.searchValue ? results : depositArray) : []}
+        entries={
+          account
+            ? props.searchValue
+              ? wrapBatchAction(results)
+              : wrapBatchAction(depositArray)
+            : []
+        }
         renderEntry={({
           id,
           amount,
@@ -172,6 +205,35 @@ const DepositTable = props => {
           rewardBalance,
           pairInfo,
         }) => {
+          if (unlockTime === '-') {
+            return [
+              <div
+                css={`
+                  margin-left: 60px;
+                  @media (max-width: 576px) {
+                    margin: 0;
+                  }
+                `}
+              >
+                <PairName name="All Deposits" subheadline="Honeyswap" />
+              </div>,
+              <PairName
+                name={'$' + amount.dollarSum}
+                subheadline={amount.amountSum}
+              />,
+              <p
+                css={`
+                  margin: auto;
+                `}
+              >
+                {unlockTime}
+              </p>,
+              <RewardComponent image={tokenImage} name={tokenName} />,
+              <p>{rewardBalance}</p>,
+              <HarvestAll id={id} onError={handleError} />,
+            ]
+          }
+
           const imgObj = {
             pair1: pairInfo ? pairInfo.token0.logoURI : undefined,
             pair2: pairInfo ? pairInfo.token1.logoURI : undefined,
@@ -208,32 +270,42 @@ const DepositTable = props => {
             <p>{unlockTimeFormatted}</p>,
             <RewardComponent image={tokenImage} name={tokenName} />,
             <p>{pendingReward}</p>,
-            <div>
-              <StyledTooltip
-                place="top"
-                type="light"
-                effect="solid"
-                backgroundColor="#aaf5d4"
-              />
-              {withdrawDisabled ? (
-                <div data-tip="Withdraw will be disabled until the unlock date is reached">
+            <>
+              <div
+                css={`
+                  margin-right: 1rem;
+                  @media (max-width: 768px) {
+                    margin-right: 0;
+                    margin-bottom: 1rem;
+                  }
+                `}
+              >
+                <StyledTooltip
+                  place="top"
+                  type="light"
+                  effect="solid"
+                  backgroundColor="#aaf5d4"
+                />
+                {withdrawDisabled ? (
+                  <div data-tip="Withdraw will be disabled until the unlock date is reached">
+                    <Withdraw
+                      id={id}
+                      disabled={withdrawDisabled}
+                      onError={handleError}
+                      opener={opener}
+                    />
+                  </div>
+                ) : (
                   <Withdraw
                     id={id}
                     disabled={withdrawDisabled}
                     onError={handleError}
                     opener={opener}
                   />
-                </div>
-              ) : (
-                <Withdraw
-                  id={id}
-                  disabled={withdrawDisabled}
-                  onError={handleError}
-                  opener={opener}
-                />
-              )}
-            </div>,
-            <Harvest id={id} onError={handleError} />,
+                )}
+              </div>
+              <Harvest id={id} onError={handleError} />
+            </>,
           ]
         }}
       />
---
+++ src/components/Farming/HavestAll/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from 'react'
+import { Button, TransactionProgress } from '@1hive/1hive-ui'
+import { useHarvest } from '../../../hooks/useHarvest'
+import { getNetworkConfig } from '../../../networks'
+import { buttonGreenCss } from '../styles'
+import { useWallet } from 'use-wallet'
+
+const HarvestAll = props => {
+  const {
+    _web3ReactContext: { chainId },
+  } = useWallet()
+  const [visible, setVisible] = useState(false)
+  const [txHash, setTxHash] = useState('')
+  const opener = useRef()
+  const harvest = useHarvest(props.id, chainId)
+  const network = getNetworkConfig(chainId)
+
+  const transactionTime = new Date()
+  transactionTime.setSeconds(transactionTime.getSeconds() + 8)
+
+  const handleHarvest = () => {
+    harvest()
+      .then(x => {
+        if (x) {
+          setTxHash(x.hash)
+          setVisible(true)
+          x.wait()
+            .then(() => {
+              setVisible(false)
+            })
+            .catch(err => {
+              props.onError(err)
+            })
+        }
+      })
+      .catch(err => {
+        props.onError(err)
+      })
+  }
+
+  return (
+    <>
+      <Button
+        css={`
+          ${buttonGreenCss}
+          width: 100%;
+        `}
+        onClick={handleHarvest}
+        label="Harvest All Rewards"
+        wide
+        ref={opener}
+      />
+      <TransactionProgress
+        transactionHash={txHash}
+        transactionHashUrl={network.txUrl + txHash}
+        progress={1}
+        visible={visible}
+        endTime={transactionTime}
+        onClose={() => setVisible(false)}
+        opener={opener}
+        slow={false}
+      />
+    </>
+  )
+}
+
+export default HarvestAll
